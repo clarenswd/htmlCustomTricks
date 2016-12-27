ba4b66731bfe64f2fd92d3ee6dec736c98304313
clarenswd <clarens007@gmail.com>
2016-12-27 19:49:00 +1000

    detect keys with JS

---
+++ js_snippets/jquery-detect-keys.js
@@ -0,0 +1,20 @@
+    //Keys Pressed Event 
+    $(document).keyup(function (e) {
+
+        // L 
+        if (e.keyCode == 76) {
+            //Do something
+        }
+        // D
+        if (e.keyCode == 68) {
+            //Do something
+        }
+        // N
+        if (e.keyCode == 78) {
+            //Do something
+        }
+	   // ESC
+        if (e.keyCode == 27) { 
+            //Do something
+        }
+    });
